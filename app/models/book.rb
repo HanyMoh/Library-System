7e4f541960a845d1e2cca60a53b8f50cc0a5595b
HanyMoh <hanymoh@msn.com>
2019-10-18 08:09:48 +0200

    point

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,13 @@ class Book < ApplicationRecord
   belongs_to :publisher
   belongs_to :book_type
   has_many :borrows
+
+  def current_stock
+    # find all borrows books where duration not ended
+    # min copies from count durations books
+  end
+
+  def stock
+    copies
+  end
 end
--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -2,13 +2,18 @@ class Borrow < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  after_validation :check_borrow_limited
+  before_save :check_borrow_limited, :check_borrow_duration
 
   def check_bodrrow_limited
-    
+    # check limit amount of books based on subscription
+    duration_limit = 2
+    if user.subscription.level > 3 && book_type.id > 2
+      duration_limit = 5
+    end
   end
 
   def check_borrow_duration
 
   end
+
 end
